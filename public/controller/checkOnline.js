daec409a56bce636803b72ff014ed27129bb3248
mivan <mivangalindo@gmail.com>
2016-11-30 15:20:11 -0600

    Solo se mantiene el public

--- public/controller/checkOnline.js
+++ public/controller/checkOnline.js
@@ -1,7 +1,7 @@
 firebase.auth().onAuthStateChanged(function(user) {
     //aqui pueden ir los permisos o mas especificaciones de sesion
         if (!user){
-            //window.location="http://localhost:5000";                        //LOCAL
+            //window.location="http://localhost:5000";                        //LOCAL Fs
             window.location="https://prueba-aleph.firebaseapp.com";         //WEB
         }
 });
