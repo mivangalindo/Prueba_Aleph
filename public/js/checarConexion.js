73d0dfc3b82d6f385179f8fe84b4e7988b56fe99
mivan <mivangalindo@gmail.com>
2017-01-31 23:32:44 -0600

    Elimiar cuentas del Auth

--- public/js/checarConexion.js
+++ public/js/checarConexion.js
@@ -2,6 +2,16 @@ firebase.auth().onAuthStateChanged(function(user) {
     //aqui pueden ir los permisos o mas especificaciones de sesion
         if (!user){
             //window.location="http://localhost:5000";                        //LOCAL Fs
-            window.location="https://aleph-b9912.firebaseapp.com";
+            window.location="https://aleph-b9912.firebaseapp.com/Sit.html";
         }
+        firebase.database().ref('deletedUsers/' + user.uid).once('value').then(function(snapshot) {
+            if(user.uid == snapshot.val().user){
+                firebase.auth().currentUser.delete().then(function() {
+                    alert('Tu cuenta ha sido eliminada.');
+                    firebase.database().ref('deletedUsers/'+user.uid).remove();
+                }, function(error) {
+                  alert('error para borrar');
+                });
+            }
+        });
 });
